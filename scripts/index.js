3219ab7ceb485ade4b3fc1829164daa788b531fe
Alvee <M.Alvee8141@gmail.com>
2023-03-20 18:21:10 +0600

    Add books & display them

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+const inputTitle = document.querySelector(".input-title")
+const inputAuthor = document.querySelector(".input-author")
+const form = document.querySelector(".form")
+
+const bookList = document.querySelector(".books-list")
+
+const books = []
+
+const reterevedBooks = localStorage.getItem("books")
+
+function renderBooks(){
+  let finalHtml = ""
+
+  books.forEach(book =>{
+    const htmlToInsert = `
+      <div>
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button> Remove </button>
+      </div>
+      <hr>
+    `
+    finalHtml += htmlToInsert;
+  })
+  bookList.innerHTML = finalHtml;
+}
+
+if(reterevedBooks){
+  books.push(...JSON.parse(reterevedBooks))
+  renderBooks()
+}
+
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+  if(inputTitle.value.length !== 0 && inputAuthor.value.length !== 0){
+    books.push({
+      title: inputTitle.value,
+      author: inputAuthor.value
+    })
+    localStorage.setItem("books", JSON.stringify(books))
+    renderBooks()
+  }
+})
