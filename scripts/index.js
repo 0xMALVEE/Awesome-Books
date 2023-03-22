3f68c14bc33fe7ca6ecdf453f4b16ba04aa70ab9
rushemaa <rushemaa@gmail.com>
2023-03-22 19:17:59 +0200

    Create single page website

--- scripts/index.js
+++ scripts/index.js
@@ -83,3 +83,19 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   booksList.addBook();
 });
+
+// Nav bar tabs
+
+const navs = document.querySelectorAll('nav ul li');
+navs.forEach((element) => {
+  element.addEventListener('click', () => {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach((tab) => {
+      tab.classList.add('hidden');
+    });
+    document.querySelector('.current').classList.remove('current');
+    const tabId = element.getAttribute('class');
+    document.querySelector(`#${tabId}`).classList.remove('hidden');
+    element.classList.add('current');
+  });
+});
